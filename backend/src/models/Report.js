329f273a571171da250c9526ec5e46d2f69b33dc
Anjula <wanjulasd@gmail.com>
2026-02-23 21:44:52 +0530

    chore: Remove unnecessary blank lines in Report model

--- backend/src/models/Report.js
+++ backend/src/models/Report.js
@@ -79,6 +79,5 @@ reportSchema.index({ location: "2dsphere" });
 reportSchema.index({ reportType: 1, status: 1 });
 
 module.exports = mongoose.model("Report", reportSchema);
-
-
+//push test
 //push test
